dc1a9e1a6abb5b76f099db9dab6a1e997a519c64
ihleung <ihfungleung@gmail.com>
2024-11-01 20:10:37 -0400

    updated graph

--- whatsAppChat.py
+++ whatsAppChat.py
@@ -1,7 +1,10 @@
 import regex
 import pandas as pd
+from collections import Counter
 import numpy as np
 from datetime import datetime
+import plotly.express as px
+import emoji
 
 def date_time(s):
     pattern = r'^\[([0-9]{4}-[0-9]{2}-[0-9]{2}), ([0-9]{2}:[0-9]{2}:[0-9]{2})\]'
@@ -44,3 +47,28 @@ print(df["Author"].unique()) #print the unique authors in the dataframe
 
 total_messages = df.shape[0] #shape is attribute of the dataframe which gives the number of rows and columns
 print(total_messages)
+print("\n")
+
+# Filter out media messages
+media_messages_df = df[df['Message'] == '<Media omitted>']
+messages_df = df.drop(media_messages_df.index)
+
+# Calculate letter and word counts
+messages_df['Letter_Count'] = messages_df['Message'].apply(len)
+messages_df['Word_Count'] = messages_df['Message'].apply(lambda s: len(s.split(' ')))
+messages_df['MessageCount'] = 1
+
+# Extract emojis from messages and create 'emoji' column
+messages_df['emoji'] = messages_df['Message'].apply(lambda x: [char for char in x if char in emoji.EMOJI_DATA])
+
+# Flatten list of emojis across messages and count them
+total_emojis_list = [e for emoji_list in messages_df['emoji'] for e in emoji_list]
+emoji_counts = Counter(total_emojis_list)
+
+# Create DataFrame for emoji frequency and visualize
+print("Emoji Distribution in WhatsApp Messages")
+print("Displaying graph in browser...")
+emoji_df = pd.DataFrame(emoji_counts.items(), columns=['emoji', 'count']).sort_values(by='count', ascending=False)
+fig = px.pie(emoji_df, values='count', names='emoji', title="Emoji Distribution in WhatsApp Messages")
+fig.update_traces(textposition='inside', textinfo='percent+label')
+fig.show()
